dab8d456409d3755923b234216794a7ffe0f49bb
is-the-biologist <iskander.said@gmail.com>
2024-04-05 10:12:00 -0400

    Refactored directory structure to fit with Snakemake best practices.

--- scripts/kspectra_pca.py
+++
@@ -1,6 +0,0 @@
-from qc_module import kmer_pca
-
-if __name__ == '__main__':
-    qc = kmer_pca()
-    qc.generatePCA(kmer_spectra=qc.combine_kmerSpectra(snkmk_in=snakemake.input[0:-1]),
-                   snkmk_out=snakemake.output, metadata=snakemake.input[-1])
